d63e434515cec58b40a9d36883282c61d7983d89
Ewout Quax <ewout.quax@bettyblocks.com>
2023-11-05 21:03:43 +0100

    refactor IntCode compiler
    Since this code will be extended upon a lot more in the coming days

--- pkg/intcoder/main.go
+++ pkg/intcoder/main.go
@@ -1,8 +1,114 @@
 package intcoder
 
+import "strconv"
+
+type Status uint
+
+const (
+	StatusRunning Status = iota + 1
+	StatusAwaitingInput
+	StatusHalted
+)
+
 type IntCoder struct {
+	status Status
+
 	idxInstruction int
 	sourceCode     map[int]int
+
+	inputs []int
+	output int
+}
+
+type ModeIndex uint
+
+const (
+	ModePosition ModeIndex = iota + 1
+	ModeImmediate
+)
+
+type Parameter struct {
+	Mode  ModeIndex
+	Index int
+	Value int
+}
+
+type CodeOperation string
+
+const (
+	OperationAdd      CodeOperation = "01"
+	OperationMultiply               = "02"
+	OperationInput                  = "03"
+	OperationOutput                 = "04"
+	OperationHalt                   = "99"
+)
+
+type StatementAdd struct {
+	size int `default:"4"`
+
+	Left   Parameter
+	Right  Parameter
+	Target Parameter
+}
+
+type StatementMultiply struct {
+	size int `default:"4"`
+
+	Left   Parameter
+	Right  Parameter
+	Target Parameter
+}
+
+type StatementInput struct {
+	size int `default:"2"`
+
+	Target Parameter
+}
+
+type StatementOutput struct {
+	size int `default:"2"`
+
+	Target Parameter
+}
+
+type StatementHalt struct {
+	size int `default:"1"`
+}
+
+func (s StatementAdd) OpCode() CodeOperation      { return OperationAdd }
+func (s StatementMultiply) OpCode() CodeOperation { return OperationMultiply }
+func (s StatementInput) OpCode() CodeOperation    { return OperationInput }
+func (s StatementOutput) OpCode() CodeOperation   { return OperationOutput }
+func (s StatementHalt) OpCode() CodeOperation     { return OperationHalt }
+
+func (s StatementAdd) exec(i *IntCoder) {
+	i.sourceCode[s.Target.Index] = s.Left.Value + s.Right.Value
+	i.idxInstruction += s.size
+}
+func (s StatementMultiply) exec(i *IntCoder) {
+	i.sourceCode[s.Target.Index] = s.Left.Value * s.Right.Value
+	i.idxInstruction += s.size
+}
+func (s StatementInput) exec(i *IntCoder) {
+	if len(i.inputs) == 0 {
+		i.status = StatusAwaitingInput
+	} else {
+		i.sourceCode[s.Target.Index] = i.inputs[0]
+		i.inputs = i.inputs[1:]
+		i.idxInstruction += s.size
+	}
+}
+func (s StatementOutput) exec(i *IntCoder) {
+	i.output = i.sourceCode[s.Target.Index]
+	i.idxInstruction += s.size
+}
+func (s StatementHalt) exec(i *IntCoder) {
+	i.status = StatusHalted
+}
+
+type Statement interface {
+	OpCode() CodeOperation
+	exec(*IntCoder)
 }
 
 func (i *IntCoder) Result() (sourceCode []int) {
@@ -14,40 +120,25 @@ func (i *IntCoder) Result() (sourceCode []int) {
 }
 
 func (i *IntCoder) Run() {
-	var running bool = true
+	i.status = StatusRunning
 
-	for running {
-		switch i.sourceCode[i.idxInstruction] {
-		case 1:
-			i.add()
-			i.idxInstruction += 4
-		case 2:
-			i.multiply()
-			i.idxInstruction += 4
-		case 99:
-			running = false
-		}
+	for i.status == StatusRunning {
+		statement := i.BuildStatement()
+		statement.exec(i)
 	}
+}
+
+func (i *IntCoder) Send(input int) {
+	i.inputs = append(i.inputs, input)
+	i.Run()
+}
+
+func (i *IntCoder) Receive() int {
+	return i.output
 }
 
 func (i *IntCoder) Set(index, instruction int) {
 	i.sourceCode[index] = instruction
-}
-
-func (i *IntCoder) add() {
-	var idxLeft int = i.sourceCode[i.idxInstruction+1]
-	var idxRight int = i.sourceCode[i.idxInstruction+2]
-	var idxTarget int = i.sourceCode[i.idxInstruction+3]
-
-	i.sourceCode[idxTarget] = i.sourceCode[idxLeft] + i.sourceCode[idxRight]
-}
-
-func (i *IntCoder) multiply() {
-	var idxLeft int = i.sourceCode[i.idxInstruction+1]
-	var idxRight int = i.sourceCode[i.idxInstruction+2]
-	var idxTarget int = i.sourceCode[i.idxInstruction+3]
-
-	i.sourceCode[idxTarget] = i.sourceCode[idxLeft] * i.sourceCode[idxRight]
 }
 
 func Compile(sourceCode []int) *IntCoder {
@@ -61,4 +152,83 @@ func Compile(sourceCode []int) *IntCoder {
 	}
 
 	return intCoder
+}
+
+func (i *IntCoder) BuildStatement() Statement {
+	buildFunc :=
+		map[int]func(*IntCoder) Statement{
+			1:  buildStatementAdd,
+			2:  buildStatementMultiply,
+			3:  buildStatementInput,
+			4:  buildStatementOutput,
+			99: buildStatementHalt,
+		}[i.sourceCode[i.idxInstruction]%100]
+
+	if buildFunc == nil {
+		panic("unknown opcode")
+	}
+
+	return buildFunc(i)
+}
+
+func buildStatementAdd(i *IntCoder) Statement {
+	return StatementAdd{
+		size:   4,
+		Left:   i.buildParameter(1),
+		Right:  i.buildParameter(2),
+		Target: i.buildParameter(3),
+	}
+}
+
+func buildStatementMultiply(i *IntCoder) Statement {
+	return StatementMultiply{
+		size:   4,
+		Left:   i.buildParameter(1),
+		Right:  i.buildParameter(2),
+		Target: i.buildParameter(3),
+	}
+}
+
+func buildStatementInput(i *IntCoder) Statement {
+	return StatementInput{
+		size:   2,
+		Target: i.buildParameter(1),
+	}
+}
+
+func buildStatementOutput(i *IntCoder) Statement {
+	return StatementOutput{
+		size:   2,
+		Target: i.buildParameter(1),
+	}
+}
+
+func buildStatementHalt(i *IntCoder) Statement {
+	return StatementHalt{
+		size: 1,
+	}
+}
+
+func (i *IntCoder) buildParameter(offset int) (p Parameter) {
+	opcode := "0000" + strconv.Itoa(i.sourceCode[i.idxInstruction])
+
+	p.Mode = convToMode(opcode[len(opcode)-2-offset])
+
+	if p.Mode == ModePosition {
+		p.Index = i.sourceCode[i.idxInstruction+offset]
+	}
+	if p.Mode == ModeImmediate {
+		p.Index = i.idxInstruction + offset
+	}
+
+	p.Value = i.sourceCode[p.Index]
+
+	return
+}
+
+func convToMode(b byte) ModeIndex {
+	return map[byte]ModeIndex{
+		'0': ModePosition,
+		'1': ModeImmediate,
+	}[b]
 }
--- pkg/intcoder/main_test.go
+++ pkg/intcoder/main_test.go
@@ -7,35 +7,72 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestBuildStatement(t *testing.T) {
+	intCoder := Compile([]int{1, 0, 0, 0, 99})
+	statement := intCoder.BuildStatement()
+	assert.Equal(t, "01", string(statement.OpCode()))
+
+	statementAdd := statement.(StatementAdd)
+	assert.Equal(t, 0, statementAdd.Left.Index)
+	assert.Equal(t, 1, statementAdd.Left.Value)
+	assert.Equal(t, ModePosition, statementAdd.Target.Mode)
+}
+
 func TestSimpleProgram(t *testing.T) {
 	var sourceCode, expectedResult []int
-	var intCode *IntCoder
+	var intCoder *IntCoder
 
 	sourceCode = []int{1, 0, 0, 0, 99}
 	expectedResult = []int{2, 0, 0, 0, 99}
-	intCode = Compile(sourceCode)
-	intCode.Run()
+	intCoder = Compile(sourceCode)
+	intCoder.Run()
 
-	assert.Equal(t, expectedResult, intCode.Result())
+	assert.Equal(t, expectedResult, intCoder.Result())
 
 	sourceCode = []int{2, 3, 0, 3, 99}
 	expectedResult = []int{2, 3, 0, 6, 99}
-	intCode = Compile(sourceCode)
-	intCode.Run()
+	intCoder = Compile(sourceCode)
+	intCoder.Run()
 
-	assert.Equal(t, expectedResult, intCode.Result())
+	assert.Equal(t, expectedResult, intCoder.Result())
 
 	sourceCode = []int{2, 4, 4, 5, 99, 0}
 	expectedResult = []int{2, 4, 4, 5, 99, 9801}
-	intCode = Compile(sourceCode)
-	intCode.Run()
+	intCoder = Compile(sourceCode)
+	intCoder.Run()
 
-	assert.Equal(t, expectedResult, intCode.Result())
+	assert.Equal(t, expectedResult, intCoder.Result())
 
 	sourceCode = []int{1, 1, 1, 4, 99, 5, 6, 0, 99}
 	expectedResult = []int{30, 1, 1, 4, 2, 5, 6, 0, 99}
-	intCode = Compile(sourceCode)
-	intCode.Run()
+	intCoder = Compile(sourceCode)
+	intCoder.Run()
 
-	assert.Equal(t, expectedResult, intCode.Result())
+	assert.Equal(t, expectedResult, intCoder.Result())
+}
+
+func TestOperationInputOutput(t *testing.T) {
+	sourceCode := []int{3, 0, 4, 0, 99}
+	intCoder := Compile(sourceCode)
+
+	intCoder.Run()
+	intCoder.Send(1337)
+	assert.Equal(t, 1337, intCoder.Receive())
+}
+
+func TestProgramWithImmediateParams(t *testing.T) {
+	var sourceCode, expectedResult []int
+	var intCoder *IntCoder
+
+	sourceCode = []int{1002, 4, 3, 4, 33}
+	expectedResult = []int{1002, 4, 3, 4, 99}
+	intCoder = Compile(sourceCode)
+	intCoder.Run()
+	assert.Equal(t, expectedResult, intCoder.Result())
+
+	sourceCode = []int{1101, 100, -1, 4, 0}
+	expectedResult = []int{1101, 100, -1, 4, 99}
+	intCoder = Compile(sourceCode)
+	intCoder.Run()
+	assert.Equal(t, expectedResult, intCoder.Result())
 }
